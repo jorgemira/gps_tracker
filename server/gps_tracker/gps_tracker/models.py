421d5f6b76819307eda0a3919bc03887b42545eb
Jorge Mira <jorge.mira.yague@gmail.com>
2020-10-22 16:37:25 +0200

    Small changes to model

--- server/gps_tracker/gps_tracker/models.py
+++ server/gps_tracker/gps_tracker/models.py
@@ -4,4 +4,9 @@ from django.db import models
 class Location(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(db_index=True, unique=True)
+
+    def __str__(self):
+        return (f"Lat: {self.latitude}, "
+                f"Long: {self.longitude} "
+                f"Time: {self.datetime.strftime('%Y-%m-%d %H:%M:%S')}")
